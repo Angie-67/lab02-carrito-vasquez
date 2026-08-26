ecbe28b1bcd3be72667408fa8a305c7a404327a2
Angie-67 <angievasquezgg@gmail.com>
2026-08-26 15:20:40 -0500

    Modele un producto, defini las variables del cliente y lista del carrito y se agrego productos

--- app/src/test/java/com/vasquez/lab02carritokotlin/ExampleUnitTest.kt
+++ app/src/test/java/com/vasquez/lab02carritokotlin/ExampleUnitTest.kt
@@ -9,12 +9,33 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 class ExampleUnitTest {
     @Test
 
     fun main() {
+        val nombreCliente = "Pedrito Flores"        // String (inferido)
+        val carrito = mutableListOf<Producto>() // lista vacía de productos
+        println("Cliente: $nombreCliente")
+        println()
+
         println("=========================================")
         println("  CARRITO DE COMPRAS - TIENDA TECSUP  ")
         println("=========================================")
+
+        carrito.add(Producto("Laptop HP", 2400.0, 1))
+        carrito.add(Producto("Mouse", 40.0, 4))
+        carrito.add(Producto("Audifonos Sony", 80.0, 2))
+        carrito.add(Producto("Teclado", 55.0, 3))
+
+        for (producto in carrito) {
+            println("Producto(s) agregado al carrito: ${producto.nombre}")
+        }
     }
 }
