d80dc49d53a4a1316337f686a55074a2d8758b49
Angie-67 <angievasquezgg@gmail.com>
2026-08-30 14:53:40 -0500

    Agregue la funcioon buscar y eliminar, ademas se actulizo el codigo

--- app/src/main/java/com/vasquez/lab02carritokotlin/Carrito.kt
+++ app/src/main/java/com/vasquez/lab02carritokotlin/Carrito.kt
@@ -22,12 +22,33 @@ fun calcularTotal(subtotal: Double, igv: Double): Double {
     val total = subtotal + igv
     return total
 }
+fun calcularDescuento(total: Double): Double {
+    return when {
+        total > 5000 -> total * 0.10
+        total > 3000 -> total * 0.05
+        else -> 0.0
+    }
+}
+fun buscarProducto(productos: List<Producto>, nombre: String): Producto? {
+    return productos.find { it.nombre.equals(nombre, ignoreCase = true) }
+}
+fun mostrarDetalle(productos: List<Producto>) {
+    println("--------- DETALLE DEL CARRITO------------")
+    var i = 1
+    for (p in productos) {
+        val importe = p.precio * p.cantidad
+        println(String.format("%d. %-20s x%d S/ %8.2f", i, p.nombre, p.cantidad, importe))
+        i++
+    }
+    println("-----------------------------------------")
+}
 
+//main
 fun main() {
     val nombreCliente = "Pedrito Flores"        // String (inferido)
     val carrito = mutableListOf<Producto>() // lista vacía de productos
 
-    println("Cliente: $nombreCliente")
+    println("Cliente: ${nombreCliente}")
     println()
 
     println("=========================================")
@@ -43,18 +64,11 @@ fun main() {
         println("Producto(s) agregado al carrito: ${producto.nombre}")
     }
 
-    fun mostrarDetalle(productos: List<Producto>) {
-        println("--------- DETALLE DEL CARRITO------------")
-        var i = 1
-        for (p in productos) {
-            val importe = p.precio * p.cantidad
-            println(String.format("%d. %-20s x%d S/ %8.2f",
-                i, p.nombre, p.cantidad, importe))
-            i++
-        }
-        println("-----------------------------------------")
+    //reporte
+    fun reporteactulizado() {
+        mostrarDetalle(carrito)
 
-        println(String.format("%-22s: %-10d","Cantidad de productos", carrito.size))
+        println(String.format("%-22s: %-10d", "Cantidad de productos", carrito.size))
 
         val subtot = calcularSubtotal(carrito)
         val IGV = calcularIGV(subtot)
@@ -67,28 +81,49 @@ fun main() {
 
         val masCaro = carrito.maxByOrNull { it.precio }
         if (masCaro != null) {
-            println("Producto mas caro: ${masCaro.nombre} " +
-                    String.format("(S/ %.2f)", masCaro.precio))
-        }
-        fun calcularDescuento(total: Double): Double {
-            return when {
-                total > 5000 -> total * 0.10
-                total > 3000 -> total * 0.05
-                else -> 0.0
-            }
+            println(
+                "Producto mas caro: ${masCaro.nombre} " +
+                        String.format("(S/ %.2f)", masCaro.precio)
+            )
         }
 
+        //descuento para mostrar en pantalla
         val desc = calcularDescuento(tot)
-        val textdesc= when {
+        val textdesc = when {
             tot > 5000 -> "10% por compra mayor a S/ 5000"
             tot > 3000 -> "5% por compra mayor a S/ 3000"
             else -> "No hay descuento"
         }
         println("Descuento aplicado: ${textdesc}")
 
+        //total a pagar
         val totpagar = tot - desc
         println(String.format("%-22s: S/ %8.2f", "TOTAL CON DESCUENTO", totpagar))
     }
+    reporteactulizado()
 
-    mostrarDetalle(carrito)
+    //otro apartado
+    println("---------------------------------------")
+
+    //buscar
+    val nombrebuscar = "mouse"
+    val encontrar = buscarProducto(carrito, nombrebuscar)
+    println("Se esta buscando: ${nombrebuscar}")
+    if (encontrar != null) {
+        println("¡Producto encontrado!")
+    } else {
+        println("No se encontró.")
+    }
+
+    //eliminar
+    val eliminar = "teclado"
+    println("Se esta eliminando el producto ${eliminar}")
+    val eliminado = carrito.removeIf {it.nombre.equals(eliminar, ignoreCase = true) }
+    if (eliminado) {
+        println("Producto eliminado de manera exitosa")
+    } else {
+        println("No se encontró.")
+    }
+    println("---------------------------------------")
+    reporteactulizado()
 }
